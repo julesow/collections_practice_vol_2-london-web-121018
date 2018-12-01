f43b44dc7860cc2f4c06d28c73710d3c791c18b3
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-12-01 23:22:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
 # your code goes here
+# def begins_with_r(array)
+#   array.each do |a| 
+#     if a[0] == ("r") 
+#       return true 
+#     else  a[0] != ("r") 
+#       return false
+#     end
+#   end 
+# end
+
+def begins_with_r(array)
+  array.any?{|a| a[0] == "r"} ? false : true
+#     return true
+#   else 
+#     return false
+# end
+end
+#########
+def contain_a(array)
+  new_array=[]
+  i= 0
+  while i < array.length
+  if  array[i].include?("a") == true
+    new_array << array[i]
+  end
+    i+=1
+
+end
+  return new_array
+end 
+#########
+def first_wa(array)
+  new_array =[]
+  i = 0
+  while i < array.length
+    if array[i].start_with?("wa")
+      return array[i]
+    end
+  i += 1
+  end
+end 
+#########
+def remove_non_strings(array)
+  array.delete_if{|a| !a.is_a?String}
+end
+#########
+
+def count_elements(array)
+end
+
+def merge_data
+  
+end
+
+def find_cool
+end
+
+def organize_schools
+end
+
+
+
